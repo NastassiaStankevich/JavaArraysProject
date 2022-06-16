085009cbfc41f610cd09a4c8f66743ba964e81e7
Paxan58 <pavelstaricov318@mail.ru>
2022-06-16 19:57:32 +0300

    add Task01

---
+++ src/by/itstep/stank/javalessons/lesson18/controller/Task01.java
@@ -0,0 +1,26 @@
+package by.itstep.stank.javalessons.lesson18.controller;
+
+import by.itstep.stank.javalessons.lesson18.view.Printer;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int size;
+
+        do {
+
+            Printer.print("Input size of array: ");
+             size = scanner.nextInt();
+        } while ( size <= 0);
+
+        int[] array = new int[size];
+
+        // logic
+
+        //output result
+
+
+    }
+}
